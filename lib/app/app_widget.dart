90dc1e4a6ae70088b4c2f9de451725db7b57ee3d
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-11-11 10:20:36 -0300

    inicio modulo auth

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
+import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
 
   @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'todo list provider',
+      routes: {
+        '/login': (_) => MultiProvider(
+              providers: [
+                Provider(create: (_) => 'Repository'),
+                Provider(create: (_) => 'Service'),
+                ChangeNotifierProvider(create: (_) => LoginController()),
+              ],
+              child: const LoginPage(),
+            ),
+        '/register': (_) => MultiProvider(
+              providers: [
+                Provider(create: (_) => 'Repository'),
+                Provider(create: (_) => 'Service'),
+                ChangeNotifierProvider(create: (_) => LoginController()),
+              ],
+              child: const LoginPage(),
+            ),
+      },
       home: SplashPage(),
     );
   }
--- lib/app/core/database/sqlite_migration_factory.dart
+++ lib/app/core/database/sqlite_migration_factory.dart
@@ -5,20 +5,20 @@ import 'package:todo_list_provider/app/core/database/migrations/migrationV3.dart
 
 class SqliteMigrationFactory {
   List<Migration> getCreateMigration() => [
-    MigrationV1(),
-    MigrationV2(),
-    MigrationV3(),
-  ];
+        MigrationV1(),
+        MigrationV2(),
+        MigrationV3(),
+      ];
 
   List<Migration> getUpgradeMigration(int version) {
     final migrations = <Migration>[];
 
-    if(version == 1) {
+    if (version == 1) {
       migrations.add(MigrationV2());
       migrations.add(MigrationV3());
     }
 
-    if(version == 2) {
+    if (version == 2) {
       migrations.add(MigrationV3());
     }
 
