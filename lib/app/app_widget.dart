158aac5d79bcaea29ccd048a64d012d9a3f88256
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-05 12:18:07 -0300

    estruturação de navegação sem contexto

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/core/navigator/todo_list_navigator.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
+import 'package:todo_list_provider/app/modules/home/home_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
@@ -33,9 +35,10 @@ class _AppWidgetState extends State<AppWidget> {
       title: 'todo list provider',
       debugShowCheckedModeBanner: false,
       theme: TodoListUiConfig.theme,
-      initialRoute: '/login',
+      navigatorKey: TodoListNavigator.navigatorKey,
       routes: {
         ...AuthModule().routers,
+        ...HomeModule().routers,
       },
       home: const SplashPage(),
     );
