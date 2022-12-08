f1ed66a15487d8f8f13ded8297c9e76d3923d5d0
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-08 17:09:32 -0300

    implementação da tela de criação de tasks

--- lib/app/modules/tasks/tasks_create_page.dart
+++ lib/app/modules/tasks/tasks_create_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/widget/todo_list_field.dart';
 import 'package:todo_list_provider/app/modules/tasks/tasks_create_controller.dart';
+import 'package:todo_list_provider/app/modules/tasks/widget/calendar_button.dart';
 
 class TasksCreatePage extends StatelessWidget {
   TasksCreateController _controller;
@@ -13,7 +16,50 @@ class TasksCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.close,
+              color: Colors.black,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+      body: Form(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Criar atividade',
+                  style: context.titleStyle.copyWith(fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 30),
+              TodoListField(label: 'label'),
+              const SizedBox(height: 20),
+              const CalendarButton(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: context.primaryColor,
+        label: const Text(
+          'Salvar Task',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {},
+      ),
     );
   }
 }
